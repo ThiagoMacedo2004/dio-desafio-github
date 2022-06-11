62ed71bda1c5719958d4120810112eb9d9103072
Thiago de Souza (Infraestrutura) <00217314@marabraz.com.br>
2022-06-11 11:52:44 -0300

    função contador com javascript

---
+++ javaScript_intro/assets/js/scrips.js
@@ -0,0 +1,42 @@
+var resultado = document.getElementById('resultado')
+var btnAdd    = document.getElementById('adicionar')
+var btnSub    = document.getElementById('subtrair')
+var produto = 0
+
+function add() {
+    produto = produto + 1
+    resultado.innerHTML = produto
+}
+
+
+function sub() {
+    produto = produto - 1
+    resultado.innerHTML = produto
+
+    
+}
+
+btnAdd.addEventListener('click', function() {
+    if(resultado.innerHTML >= 10) {
+       btnAdd.disabled  = true
+    } else {
+        btnAdd.disabled = false
+    }
+
+    if(resultado.innerHTML > 0) {
+        btnSub.disabled = false
+    }
+}, false)
+
+
+btnSub.addEventListener('click', function() {
+    if(resultado.innerHTML == 0) {
+        btnSub.disabled = true
+    } else {
+        btnSub.disabled = false
+    }
+
+    if(resultado.innerHTML < 10) {
+        btnAdd.disabled = false
+    }
+})
