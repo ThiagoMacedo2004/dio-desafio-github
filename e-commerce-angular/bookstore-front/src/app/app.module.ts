28db974c30485d4100dd38e89cc82f8427f1e1d6
Thiago Macedo <tsouzamacedo@gmail.com>
2022-07-31 21:06:49 -0300

    projeto angular e-commerce livros, sem o backend

--- e-commerce-angular/bookstore-front/src/app/app.module.ts
+++ e-commerce-angular/bookstore-front/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
+import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    BookstoreAppComponent,
+    ProductListComponent,
+    FiltersComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
